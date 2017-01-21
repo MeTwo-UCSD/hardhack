20e6718c9bb6cd361d6751ab87608a542819e865
Yiyu Zhu <smallzzy@outlook.com>
2017-01-21 09:59:51 -0800

    working button, need to fix camera

--- app.py
+++ app.py
@@ -1,12 +1,29 @@
+from __future__ import print_function
 import os
-from flask import Flask, render_template, Response
-from dummy_camera import Camera
+from flask import Flask, render_template, Response, request, jsonify
+from camera import Camera
+import Queue
+from raven.contrib.flask import Sentry
+import time
+
+frameRateLimit = 20
 
 app = Flask(__name__)
+sentry = Sentry(app, dsn='https://b705ec878ec74aae84c1b26a4194b612:6288485481504394a071e8ba726a84a4@sentry.io/131345')
+command = Queue.Queue()
+
+LEFT, RIGHT, UP, DOWN, RESET = "left", "right", "up", "down", "reset"
+AVAILABLE_COMMANDS = {
+    'Left': LEFT,
+    'Right': RIGHT,
+    'Up': UP,
+    'Down': DOWN,
+    'Reset': RESET
+}
 
 @app.route('/')
 def hello():
-    return render_template('index.html')
+		return render_template('index.html', commands=AVAILABLE_COMMANDS)
 
 def gen(camera):
 	while True:
@@ -16,8 +33,20 @@ def gen(camera):
 
 @app.route('/video_feed')
 def video_feed():
-    return Response(gen(Camera()),
-                    mimetype='multipart/x-mixed-replace; boundary=frame')
+	return Response(gen(Camera()),
+		mimetype='multipart/x-mixed-replace; boundary=frame')
+
+
+@app.route('/gpio')
+def gpio():
+	#try:
+	lang = request.args.get('proglang', 0, type=str)
+	if lang.lower() == 'python':
+		return jsonify(result='You are wise')
+	else:
+		return jsonify(result='Try again.')
+	#except Exception as e:
+	#	return str(e)
 
 if __name__ == '__main__':
-	app.run(host=os.getenv('IP', '0.0.0.0'),port=int(os.getenv('PORT', 8080)))
+	app.run(host=os.getenv('IP', '0.0.0.0'),port=int(os.getenv('PORT', 8080)),debug = True)
