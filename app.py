f59ac587034420526738de12fab1d066ce16ceb8
Yiyu Zhu <smallzzy@outlook.com>
2017-02-05 16:07:17 -0800

    streaming working version using webm
    http://wiki.webmproject.org/adaptive-streaming/instructions-to-do-webm-live-streaming-via-dash

--- app.py
+++ app.py
@@ -1,12 +1,18 @@
+"""
+flask + ffmpeg
+"""
+
 from __future__ import print_function
 import os
-from flask import Flask, render_template, Response, request, jsonify
-from camera import Camera
-#from buttonScannerHandler import buttonScannerHandler
-#from raven.contrib.flask import Sentry
+from flask import *
 import time
 import threading
-import Queue
+import queue as Queue
+
+from dummy_camera import Camera
+
+#from buttonScannerHandler import buttonScannerHandler
+#from raven.contrib.flask import Sentry
 
 frameRateLimit = 20
 commandQueue = Queue.Queue(maxsize=0)
@@ -20,25 +26,29 @@ AVAILABLE_COMMANDS = {
     'Right': RIGHT,
     'Forward': FORWARD,
     'Backward': BACKWARD,
-		'Pause': PAUSE,
+    'Pause': PAUSE,
     'Stop': STOP
 }
 
+
 @app.route('/')
-def hello():
-		return render_template('index.html', commands=AVAILABLE_COMMANDS)
+def index():
+    return render_template('index2.html', commands=AVAILABLE_COMMANDS)
 
-def gen(camera):
-	while True:
-		time.sleep(1/10)
-		frame = camera.get_frame()
-		yield (b'--frame\r\n'
-			b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
 
-@app.route('/video_feed')
-def video_feed():
-	return Response(gen(Camera()),
-		mimetype='multipart/x-mixed-replace; boundary=frame')
+# def gen(camera):
+#     while True:
+#         time.sleep(1 / 10)
+#         frame = camera.get_frame()
+#         yield (b'--frame\r\n'
+#                b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+
+
+# @app.route('/video_feed')
+# def video_feed():
+#     return Response(gen(Camera()),
+#                     mimetype='multipart/x-mixed-replace; boundary=frame')
+
 
 def shutdown_server():
     func = request.environ.get('werkzeug.server.shutdown')
@@ -46,43 +56,50 @@ def shutdown_server():
         raise RuntimeError('Not running with the Werkzeug Server')
     func()
 
+
 @app.route('/shutdown', methods=['POST'])
 def shutdown():
     shutdown_server()
     return 'Server shutting down...'
 
+
 @app.route('/gpio')
 def gpio():
-	try:
-		lang = request.args.get('command', 0, type=str)
-		lang = lang.lower()
-		if lang == LEFT:
-			commandQueue.put(lang)
-			return jsonify(result='left')
-		elif lang == RIGHT:
-			commandQueue.put(lang)
-			return jsonify(result='right')
-		elif lang == FORWARD:
-			commandQueue.put(lang)
-			return jsonify(result='forward')
-		elif lang == BACKWARD:
-			commandQueue.put(lang)
-			return jsonify(result='backward')
-		elif lang == PAUSE:
-			commandQueue.put(lang)
-			return jsonify(result='pause')
-		elif lang == STOP:
-			return jsonify(result='Stop')
-		else:
-			return jsonify(result='Try again.')
-	except Exception as e:
-		return str(e)
+    try:
+        lang = request.args.get('command', 0, type=str)
+        lang = lang.lower()
+        if lang == LEFT:
+            commandQueue.put(lang)
+            return jsonify(result='left')
+        elif lang == RIGHT:
+            commandQueue.put(lang)
+            return jsonify(result='right')
+        elif lang == FORWARD:
+            commandQueue.put(lang)
+            return jsonify(result='forward')
+        elif lang == BACKWARD:
+            commandQueue.put(lang)
+            return jsonify(result='backward')
+        elif lang == PAUSE:
+            commandQueue.put(lang)
+            return jsonify(result='pause')
+        elif lang == STOP:
+            return jsonify(result='Stop')
+        else:
+            return jsonify(result='Try again.')
+    except Exception as e:
+        return str(e)
+
+@app.route('/live/<path:filename>')
+def send_live(filename):
+    return send_from_directory('live', filename)
 
 def start_server(app):
-		app.run(host=os.getenv('IP', '0.0.0.0'),port=int(os.getenv('PORT', 8080)), threaded=True)
+    app.run(host=os.getenv('IP', '0.0.0.0'), port=int(
+        os.getenv('PORT', 8080)), threaded=True)
 
 if __name__ == '__main__':
-	#t = threading.Thread(target=buttonScannerHandler, args=(commandQueue,))
-	#t.start()
-	server_thread = threading.Thread(target=start_server, args=(app, ))
-	server_thread.start()
+    #t = threading.Thread(target=buttonScannerHandler, args=(commandQueue,))
+    # t.start()
+    server_thread = threading.Thread(target=start_server, args=(app, ))
+    server_thread.start()
--- camera_ff.py
+++ camera_ff.py
@@ -7,14 +7,18 @@ require python > 3.3
 
 from __future__ import print_function
 import subprocess
-import sys, os
+import sys
+import os
 import shutil
 import platform
+import shlex
+
 
 class Camera:
     """
     capture video through ffmepg command line
     """
+
     def __init__(self):
         # require python > 3.3
         try:
@@ -34,30 +38,45 @@ class Camera:
 
         # input module based on system
         self._command = [self.bin_path, '-y']
-        #if platform.system() == 'Windows':
+        # if platform.system() == 'Windows':
         #    self._command.extend(['-f', 'dshow'])
 
-
     def run(self):
-        #ffmpeg -list_devices true -f dshow -i dummy
-        #ffmpeg -f dshow -list_options true -i video="Integrated Camera"
-        #ffmpeg -f dshow -video_size 1920x1080 -framerate 30 -pixel_format
-        #video4linux2
+        # ffmpeg -list_devices true -f dshow -i dummy
+        # ffmpeg -f dshow -list_options true -i video="Integrated Camera"
+        # ffmpeg -f dshow -video_size 1920x1080 -framerate 30 -pixel_format
+        # video4linux2
         """
         todo: add configuration and device selection
         """
+
+        VP9_LIVE_PARAMS = shlex.split(
+            "-speed 6 -tile-columns 4 -frame-parallel 1 -threads 8 -static-thresh 0 -max-intra-rate 300 -deadline realtime -lag-in-frames 0 -error-resilient 1")
+
         # input video device
-        self._command.extend(['-f', 'dshow', '-vcodec', 'h264', '-video_size', '1920x1080', '-framerate', '30', '-i', 'video=HD Pro Webcam C920'])
-        self._command.extend(['-f', 'dshow', '-sample_size', '16', '-sample_rate', '44100', '-channels', '1', '-i', 'audio=Microphone (HD Pro Webcam C920)'])
-        # input video size
-        #input audio size
-        
-        self._command.extend(['-map', '0:0', '-map', '1:0'])
-        self._command.extend(['-vcodec', 'libx264'])
-        self._command.extend(['-acodec', 'aac', '-ab', '128k', '-ar', '44100'])
-        self._command.extend(['-maxrate', '750k', '-bufsize', '4000k', 'out.mp4'])
+        self._command.extend(['-f', 'dshow', '-vcodec', 'h264', '-video_size',
+                              '1920x1080', '-framerate', '30', '-i', 'video=HD Pro Webcam C920'])
+        # input audio device
+        self._command.extend(['-f', 'dshow', '-sample_size', '16', '-sample_rate',
+                              '44100', '-channels', '1', '-i', 'audio=Microphone (HD Pro Webcam C920)'])
+        # map input
+        self._command.extend(['-map', '0:0', '-pix_fmt', 'yuv420p',
+                              '-c:v', 'libvpx-vp9', '-s', '1280x720', '-keyint_min', '60', '-g', '60', '-b:v', '3000k'])
+
+        self._command.extend(VP9_LIVE_PARAMS)
+
+        self._command.extend(['-f', 'webm_chunk', '-header', '/live/glass_360.hdr',
+                              '-chunk_start_index', '1', '/live/glass_360_%d.chk'])
+
+        # , '-map', '1:0'])
+        # # output video & audio format
+        # self._command.extend(['-vcodec', 'libx264'])
+        # self._command.extend(['-acodec', 'aac', '-ab', '128k', '-ar', '44100'])
+        # self._command.extend(
+        #     ['-maxrate', '750k', '-bufsize', '4000k', 'out.mp4'])
         try:
-            self._process = subprocess.Popen(self._command, stdin=subprocess.PIPE)
+            self._process = subprocess.Popen(
+                self._command, stdin=subprocess.PIPE)
         except subprocess.CalledProcessError as e:
             out_bytes = e.output
 
@@ -74,5 +93,5 @@ if __name__ == "__main__":
     test_cam = Camera()
     t = Thread(target=test_cam.run, daemon=True)
     t.start()
-    time.sleep(10)
+    time.sleep(30)
     test_cam.terminate()
