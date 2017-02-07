f35616c31f51762eb9213528124bdee470829f5e
Yiyu Zhu <smallzzy@outlook.com>
2017-02-06 18:31:51 -0800

    remove dummy camera

--- dummy_camera.py
+++
@@ -1,8 +0,0 @@
-from time import time
-
-class Camera(object):
-    def __init__(self):
-        self.frames = [open('img/' + f + '.jpg', 'rb').read() for f in ['1', '2', '3']]
-
-    def get_frame(self):
-        return self.frames[int(time()) % 3]
